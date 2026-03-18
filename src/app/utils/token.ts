4b498d90767e795845e4774ce7406de9ab996a2a
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-03-19 00:23:19 +0600

    create   setAccessTokenCookie,     setRefreshTokenCookie,     setBetterAuthSessionCookie

--- src/app/utils/token.ts
+++ src/app/utils/token.ts
@@ -1,6 +1,8 @@
 import { JwtPayload, SignOptions } from "jsonwebtoken";
 import { jwtUtils } from "./jwt";
 import { envConfig } from "../../config/env";
+import { cookieUtils } from "./cookie";
+import { Response } from "express";
 const getAccessToken = (payload: JwtPayload) => {
     const accessToken = jwtUtils.createToken(
         payload, envConfig.ACCESS_TOKEN_SECRET, {
@@ -17,8 +19,40 @@ const getRefreshToken = (payload: JwtPayload) => {
     )
     return refreshToken
 }
+const setAccessTokenCookie = (res: Response, token: string) => {
+    cookieUtils.setCookie(res, "accessToken", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: "/",
+        maxAge: 60 * 60 * 60 * 24 * 1000
+
+    })
+}
+const setRefreshTokenCookie = (res: Response, token: string) => {
+    cookieUtils.setCookie(res, "refreshToken", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: '/',
+        maxAge: 60 * 60 * 60 * 24 * 1000 * 7
+    })
+}
+const setBetterAuthSessionCookie = (res: Response, token: string) => {
+    cookieUtils.setCookie(res, "better-auth-session_token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: '/',
+        maxAge: 60 * 60 * 60 * 24
+    })
+}
 export const tokenUtils = {
     getAccessToken,
     getRefreshToken,
+    setAccessTokenCookie,
+    setRefreshTokenCookie,
+    setBetterAuthSessionCookie
+
 
 }
